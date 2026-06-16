64e0867c45234c697d4a64ee3b078283cea2a4f0
Gabriel Pereira <gabrielpnascimento00@gmail.com>
2026-06-16 16:49:53 -0300

    Mudei a lógica da mudança de sprites para uma variavel de 'state' que diz qual animação será executada, além disso criei os métodos na função Game que indicam cada uma das telas, Obs: Pedro muda os sprites do personagem pfv

---
+++ entities.py
@@ -0,0 +1,281 @@
+import pygame
+import constantes as cst
+
+class Entidade:
+    def __init__(self, pos):
+        self.pos = list(pos)
+        self.animacao = None
+
+        self.vel_x = 0
+        self.vel_y = 0
+
+        self.contagem_frames = 0
+
+class Player(Entidade):
+    def __init__(self, pos, screen, vida):
+        super().__init__(pos)
+
+        self.vida = vida
+        self.invulnerabilidade = 0
+
+        self.screen = screen
+
+        self.colisao = pygame.Rect(pos[0], pos[1], 80, 80)
+
+        self.contagem_moeda = 0
+
+        self.hitbox_atq = None
+
+        self.state = 'idle'
+        self.state_antes = 'idle'
+        self.state_antes_dash = 'idle'
+        self.cooldown_dash = 0
+
+        self.cooldown_atq = 0
+
+        self.pulo_duplo = False
+
+        self.andando_direita = True
+        self.no_chao = False
+
+        self.vel_x = 0
+        self.vel_y = 0
+
+        self.time_dash = 0
+
+        self.pulo = 0
+
+        self.contagem_frames = 0
+
+        self.som_dano = pygame.mixer.Sound('Assets/Sons/tomou_dano.wav')
+
+        self.coracao_cheio = pygame.transform.scale(pygame.image.load('Assets/Personagem/vida_cheia.png'), (64, 64))
+        self.coracao_vazio = pygame.transform.scale(pygame.image.load('Assets/Personagem/vida_vazia.png'), (64, 64))
+
+        self.idle = [
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/idle_00.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/idle_01.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/idle_02.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/idle_03.png').convert_alpha(), (80, 80))
+        ]
+
+        self.andando = [
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/andando_01.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/andando_00.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/andando_02.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/andando_03.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/andando_04.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/andando_05.png').convert_alpha(), (80, 80))
+        ]
+
+        self.pulando = [
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/pulando_00.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/pulando_01.png').convert_alpha(), (80, 80))
+        ]
+
+        self.puloduplo = [
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/puloduplo_00.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/puloduplo_01.png').convert_alpha(), (80, 80)),
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/puloduplo_02.png').convert_alpha(), (80, 80))
+        ]
+
+        self.dash = [
+            pygame.transform.scale(pygame.image.load('Assets/Personagem/dash_00.png').convert_alpha(), (120, 120))
+        ]
+
+        self.animacao = self.idle
+        self.state = 'idle'
+
+    #Muda as animações e os estados
+    def processar_evento(self, event):
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_w and self.no_chao:
+                self.pulo = cst.BUFFERPULO
+
+            elif event.key == pygame.K_w and self.pulo_duplo and self.vel_y >= 0:
+                self.pulo = cst.BUFFERPULO
+                self.state = 'pulo duplo'
+
+            if event.key == pygame.K_p and self.cooldown_dash == 0:
+                self.time_dash = cst.TEMPODASH
+                self.cooldown_dash = cst.COOLDOWN_DASH
+                self.state_antes_dash = self.state
+                self.state = 'dash'
+
+            if event.key == pygame.K_o and self.state != 'dash' and self.cooldown_atq == 0:
+                self.atacar()
+                self.cooldown_atq = cst.COOLDOWN_ATQ
+                self.vel_x = 0
+
+    def atualizar_animacao(self):
+
+        state_antes = self.state
+
+        #Atualizando os estados para mudar a animação
+        if self.vel_y != 0 and self.state != 'pulo duplo' and self.state != 'dash':
+            self.state = 'pulando'
+        elif self.vel_x != 0 and self.vel_y == 0 and self.state != 'dash':
+            self.state = 'andando'
+        elif self.vel_x == 0 and self.vel_y == 0 and self.state != 'dash':
+            self.state = 'idle'
+
+        if self.state != state_antes:
+            self.contagem_frames = 0
+
+        if self.no_chao and self.state == 'pulando' and self.vel_y == 0:
+            self.state = 'idle'
+
+        if self.state == 'dash':
+            self.animacao = self.dash
+        elif self.state == 'andando':
+            self.animacao = self.andando
+        elif self.state == 'pulando':
+            self.animacao = self.pulando
+        elif self.state == 'idle':
+            self.animacao = self.idle
+        elif self.state == 'pulo duplo':
+            self.animacao = self.puloduplo
+
+        if self.state != 'pulando':
+            self.contagem_frames += 0.2
+            if self.contagem_frames >= len(self.animacao):
+                self.contagem_frames = 0
+        else:
+            if self.contagem_frames < 1:
+                self.contagem_frames += 0.2
+
+    def movimento(self):
+
+        self.y_anterior = self.pos[1]
+
+        self.vel_x = 0
+
+        if self.cooldown_dash > 0:
+            self.cooldown_dash -= 1
+
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_a]:
+            self.vel_x = -cst.VEL_PERSONAGEM
+            self.andando_direita = False
+        elif teclas[pygame.K_d]:
+            self.vel_x = cst.VEL_PERSONAGEM
+            self.andando_direita = True
+        if self.pulo > 0:
+            self.pulo -= 1
+
+        if self.pulo > 0 and self.no_chao:
+            self.vel_y = cst.PULO
+
+        elif self.pulo > 0 and self.pulo_duplo and self.vel_y >= 0:
+            self.vel_y = cst.PULO
+            self.pulo_duplo = False
+
+        if self.time_dash > 0:
+            self.time_dash -= 1
+            self.vel_y = 0
+            if self.andando_direita:
+                self.vel_x = cst.VELDASH
+            elif not self.andando_direita:
+                self.vel_x = -cst.VELDASH
+            if self.time_dash == 0:
+                self.state = self.state_antes_dash
+
+        self.pos[0] += self.vel_x
+
+        #Gravidade
+
+        if self.state != 'dash':
+            self.vel_y += cst.GRAVIDADE
+            self.pos[1] += self.vel_y
+
+        self.colisao.x = self.pos[0]
+        self.colisao.y = self.pos[1]
+
+    def atacar(self):
+        if self.andando_direita:
+            self.hitbox_atq = pygame.Rect(self.pos[0] + 140, self.pos[1], 50, 128)
+            pygame.draw.rect(self.screen, cst.AMARELO, self.hitbox_atq)
+        else:
+            self.hitbox_atq = pygame.Rect(self.pos[0] - 50, self.pos[1], 50, 128)
+            pygame.draw.rect(self.screen, cst.AMARELO, self.hitbox_atq)
+
+    def atualizar_vida(self):
+        if self.vida == 3:
+            self.screen.blit(self.coracao_cheio, (15, 15))
+            self.screen.blit(self.coracao_cheio, (45, 15))
+            self.screen.blit(self.coracao_cheio, (75, 15))
+        elif self.vida == 2:
+            self.screen.blit(self.coracao_cheio, (15, 15))
+            self.screen.blit(self.coracao_cheio, (45, 15))
+            self.screen.blit(self.coracao_vazio, (75, 15))
+        elif self.vida == 1:
+            self.screen.blit(self.coracao_cheio, (15, 15))
+            self.screen.blit(self.coracao_vazio, (45, 15))
+            self.screen.blit(self.coracao_vazio, (75, 15))
+        else:
+            self.screen.blit(self.coracao_vazio, (15, 15))
+            self.screen.blit(self.coracao_vazio, (45, 15))
+            self.screen.blit(self.coracao_vazio, (75, 15))
+
+    def desenhar(self):
+
+        frame = self.animacao[int(self.contagem_frames)]
+
+        if self.invulnerabilidade > 0:
+
+            if (self.invulnerabilidade // 5) % 2 == 0:
+                frame.set_alpha(100)
+            else:
+                frame.set_alpha(255)
+        else:
+            frame.set_alpha(255)
+
+        if not self.andando_direita:
+            frame = pygame.transform.flip(frame, True, False)
+
+        self.screen.blit(frame, self.pos)
+
+class Moeda(Entidade):
+    def __init__(self, pos, screen):
+        super().__init__(pos)
+        self.screen = screen
+
+        self.som_moeda = pygame.mixer.Sound('Assets/Sons/som_moeda.wav')
+
+        self.colisao = pygame.Rect(pos[0], pos[1], 25, 25)
+
+        self.animacao = [
+            pygame.transform.scale(pygame.image.load('Assets/Coletáveis/moeda_00.png').convert_alpha(), (64, 64)),
+            pygame.transform.scale(pygame.image.load('Assets/Coletáveis/moeda_01.png').convert_alpha(), (64, 64)),
+            pygame.transform.scale(pygame.image.load('Assets/Coletáveis/moeda_02.png').convert_alpha(), (64, 64)),
+            pygame.transform.scale(pygame.image.load('Assets/Coletáveis/moeda_03.png').convert_alpha(), (64, 64)),
+            pygame.transform.scale(pygame.image.load('Assets/Coletáveis/moeda_04.png').convert_alpha(), (64, 64)),
+            pygame.transform.scale(pygame.image.load('Assets/Coletáveis/moeda_05.png').convert_alpha(), (64, 64))
+        ]
+
+        self.contagem_frames = 0
+    
+    def desenhar(self):
+
+        self.contagem_frames += 0.1
+
+        if self.contagem_frames >= len(self.animacao):
+            self.contagem_frames = 0
+
+        frame = self.animacao[int(self.contagem_frames)]
+
+        self.screen.blit(frame, self.pos)
+
+class Espinho(Entidade):
+    def __init__(self, pos, screen):
+        super().__init__(pos)
+
+        self.screen = screen
+
+        self.colisao = pygame.Rect(pos[0], pos[1] - 32, 30, 30)
+
+    def desenhar(self):
+        self.imagem = pygame.image.load('Assets/Ambiente/espinho.png')
+        self.screen.blit(self.imagem, (self.pos[0], self.pos[1] - 32))
---
+++ game.py
@@ -0,0 +1,155 @@
+import pygame
+import sys
+import constantes as cst
+from entities import Player
+from entities import Moeda
+from entities import Espinho
+
+pygame.init()
+pygame.mixer.init()
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((cst.SCREEN_WIDTH, cst.SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.contagem_frames = 0
+
+        self.tela_anterior = None
+        self.tela_atual = 'menu inicial'
+
+        self.estado = 'jogando'
+
+        self.fonte = pygame.font.Font(None, 50)
+
+        self.plataformas = [
+            pygame.Rect(0, 800, 1800, 160), #Chão
+        ]
+
+        self.msg = pygame.transform.scale(pygame.image.load('Assets/mensagem_inicial.png'), (1500, 512))
+
+        self.tela_menu = pygame.transform.scale(pygame.image.load('Assets/Tela inicial.png'), (cst.SCREEN_WIDTH, cst.SCREEN_HEIGHT))
+
+        self.tela_tutorial = pygame.transform.scale(pygame.image.load('Assets/Tela tutorial.png'), (cst.SCREEN_WIDTH, cst.SCREEN_HEIGHT))
+
+        self.chao = pygame.transform.scale(pygame.image.load('Assets/chao.png'), (cst.SCREEN_WIDTH, 200))
+
+    def MenuInicial(self):
+
+        while True:
+            self.screen.blit(self.tela_menu, (0, 0))
+            self.screen.blit(self.msg, (150, 500))
+            pygame.display.update()
+
+            self.clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        return self.TelaTutorial()
+                if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+
+    def TelaTutorial(self):
+
+        player = Player((100, 600), self.screen, 3)
+
+        while True:
+
+            #Ordem de ações para fazer as telas:
+            #1: Desenhar o cenário e o chão (se tiver plataformas é só botar la na lista do self.plataformas pra adicionar as colisões delas) e quando forem checar a
+            # colisão com o boneco, só vc botar self.plataformas[indice:indice] pra determinar quais colisões serão consideradas naquela tela específica
+
+            #2: Atualizar as informações do jogador se ele ainda não tiver morrido
+
+            #3: Checar as colisões com as plataformas (chão incluso)
+
+            #4: Por fim desenhar o player e no final de tudo ver se ele morre para chamar a função reiniciar(), que coloca o player de volta na primeira tela do jogo
+
+            #Observações:
+            #Se forem adicionar alguma constante nova, modifiquem o arquivo constantes.py, e para usar essa constante em outros arquivos é só usar 'cst.(nome da constante)'
+            #Para criar os coletáveis é só ir no arquivo entities.py, criar uma nova classe que herda da classe Entidade(que tem apenas posição e colisão) e adicionar atributos que serão necessários
+            #Pedro Alves pra tu fazer as mudanças de sprites pode ir na entites.py na classe Player e ir no método de atualizar_animacao() que la é onde tudo acontece
+
+            self.screen.fill((0, 0, 0))
+
+            self.screen.blit(self.tela_tutorial, (0, 0))
+            self.screen.blit(self.chao, (0, 800))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if self.estado == 'Game over':
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_r:
+                                self.reiniciar(player)
+
+                elif self.estado == 'jogando':
+                    player.processar_evento(event)
+
+            if self.estado == 'jogando':
+                player.atualizar_animacao()
+                player.movimento()
+
+            if player.invulnerabilidade > 0:
+                player.invulnerabilidade -= 1
+
+            player.no_chao = False
+
+            player.atualizar_vida()
+
+            if player.cooldown_atq > 0:
+                player.cooldown_atq -= 1
+
+            #Ver a colisão do pé do personagem e só considerar ela para ver se ele está em cima de uma plataforma
+            for plataforma in self.plataformas:
+                pe_anterior = player.y_anterior + player.colisao.height
+                pe_atual = player.colisao.bottom
+
+                #Isso vê se o personagem está exatamente em cima da plataforma analisando as bordas dos sprites
+                if player.colisao.right > plataforma.left and player.colisao.left < plataforma.right and pe_anterior <= plataforma.top and pe_atual >= plataforma.top and player.vel_y >= 0:
+                    player.vel_y = 0
+                    player.pos[1] = plataforma.top - player.colisao.height
+                    player.colisao.y = player.pos[1]
+                    player.no_chao = True
+                    player.pulo_duplo = True
+                    break
+
+            if self.estado == 'jogando':
+                player.desenhar()
+
+            if player.vida <= 0:
+                self.estado = 'Game over'
+                texto = self.fonte.render("GAME OVER - Aperte R para reiniciar", True, (255, 255, 255))
+                self.screen.blit(texto, (400, 300))
+            pygame.display.update()
+            self.clock.tick(60)
+
+    def Tela_1(self):
+
+        while True:
+            self.screen.blit(self.chao, (0, 800))
+
+    def Reiniciar(self, player):
+            player.pos = [100, 500]
+            player.colisao.x = 100
+            player.colisao.y = 500
+
+            player.vida = 3
+            player.vel_x = 0
+            player.vel_y = 0
+            player.contagem_moeda = 0
+            player.invulnerabilidade = 0
+
+            self.moedas = [
+                Moeda((865, 525), self.screen),
+                Moeda((1215, 450), self.screen)
+            ]
+
+            return self.TelaTutorial()
+
+game = Game()
+game.MenuInicial()
